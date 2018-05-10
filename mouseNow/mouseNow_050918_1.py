91c454a5f7046fd9887d43d8f7bcb25d1c42760b
Sunny Lam <sunny.lam@gmail.com>
2018-05-09 22:12:13 -0400

    revised code

--- mouseNow/mouseNow_050918_1.py
+++ mouseNow/mouseNow_050918_1.py
@@ -17,10 +17,9 @@ try:
 	while True:
 		# get and print the mouse coordinates
 		x,y = pyautogui.position()
-		positionStr = "X:  " + str(x).rjust(4) + "Y:  " + str(y).rjust(4)
+		positionStr = "X:  " + str(x).rjust(4) + " Y:  " + str(y).rjust(4)
+		# logging.debug( positionStr )
+		print(positionStr,end="")
+		print( "\b" * len(positionStr),end="",flush=True )
 except KeyboardInterrupt:
 	print("\nDone.")
-
-print(positionStr,end="")
-print( "\b" * len(positionStr),end="",flush=True )
-
