8da35679f67440f1a56962e9ac270a3ea938ca3b
Sunny Lam <sunny.lam@gmail.com>
2018-05-15 11:00:18 -0400

    re-enabled logging

--- mouseNow/mouseNow_050918_1.py
+++ mouseNow/mouseNow_050918_1.py
@@ -7,7 +7,7 @@
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format=" %(asctime)s - %(levelname)s - %(message)s")
-# logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 import pyautogui
 
