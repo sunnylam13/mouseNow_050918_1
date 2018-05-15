8e85437939ecce71444854973417772de0891c76
Sunny Lam <sunny.lam@gmail.com>
2018-05-15 10:57:10 -0400

    revised code to analyze colour at cursor location

--- mouseNow/mouseNow_050918_1.py
+++ mouseNow/mouseNow_050918_1.py
@@ -19,6 +19,13 @@ try:
 		x,y = pyautogui.position()
 		positionStr = "X:  " + str(x).rjust(4) + " Y:  " + str(y).rjust(4)
 		# logging.debug( positionStr )
+		
+		# color analysis
+		pixelColor = pyautogui.screenshot().getpixel( (x,y) )
+		positionStr += ' RGB: (' + str( pixelColor[0] ).rjust(3)
+		positionStr += ', ' + str( pixelColor[1] ).rjust(3)
+		positionStr += ', ' + str( pixelColor[2] ).rjust(3) + ')'
+
 		print(positionStr,end="")
 		print( "\b" * len(positionStr),end="",flush=True )
 except KeyboardInterrupt:
